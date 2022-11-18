b882a3b17edfcd2a3adf1f73b04b7ac443866c3b
Murtazin <megashlang>
2022-11-18 23:19:49 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate void MathDelegate(int a, int b);
+
+namespace lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathDelegate del;
+            del = (a, b) => Console.WriteLine(a + b);
+            del += (a, b) => Console.WriteLine(a - b);
+            del += (a, b) => Console.WriteLine(a * b);
+            del(10, 3);
+
+            Console.ReadLine();
+        }
+    }
+}
